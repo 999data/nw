717ba0c6751f7bbd2d52018e0fd255b0b8b6801c
999data <user912209@gmail.com>
2024-06-22 11:05:33 +0530

    Update ptop.cc

--- ptop.cc
+++ ptop.cc
@@ -1,106 +1,62 @@
 #include "ns3/core-module.h"
-#include "ns3/network-module.h"
-#include "ns3/csma-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
+#include "ns3/network-module.h" 
+#include "ns3/internet-module.h" 
+#include "ns3/point-to-point-module.h" 
 #include "ns3/applications-module.h"
-#include "ns3/ipv4-global-routing-helper.h"
-//netanimation
 #include "ns3/netanim-module.h"
-#include "ns3/mobility-module.h"
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("SecondScriptExample");
+NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
 
-int 
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-  bool verbose = true;
-  uint32_t nCsma = 3;
+  CommandLine cmd; 
+  cmd.Parse (argc, argv); 
+  Time::SetResolution (Time::NS);
 
-  CommandLine cmd (__FILE__);
-  cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
-  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO); 
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-  cmd.Parse (argc,argv);
-
-  if (verbose)
-    {
-      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
-    }
-
-  nCsma = nCsma == 0 ? 1 : nCsma;
-
-  NodeContainer p2pNodes;
-  p2pNodes.Create (2);
-
-  NodeContainer csmaNodes;
-  csmaNodes.Add (p2pNodes.Get (1));
-  csmaNodes.Create (nCsma);
+  NodeContainer nodes; 
+  nodes.Create (2);
 
   PointToPointHelper pointToPoint;
   pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
   pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
-  NetDeviceContainer p2pDevices;
-  p2pDevices = pointToPoint.Install (p2pNodes);
+  NetDeviceContainer devices;
+  devices = pointToPoint.Install (nodes);
 
-  CsmaHelper csma;
-  csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
-  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
-
-  NetDeviceContainer csmaDevices;
-  csmaDevices = csma.Install (csmaNodes);
-
-  InternetStackHelper stack;
-  stack.Install (p2pNodes.Get (0));
-  stack.Install (csmaNodes);
+  InternetStackHelper stack; 
+  stack.Install (nodes);
 
   Ipv4AddressHelper address;
   address.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer p2pInterfaces;
-  p2pInterfaces = address.Assign (p2pDevices);
-
-  address.SetBase ("10.1.2.0", "255.255.255.0");
-  Ipv4InterfaceContainer csmaInterfaces;
-  csmaInterfaces = address.Assign (csmaDevices);
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);
 
   UdpEchoServerHelper echoServer (9);
-
-  ApplicationContainer serverApps = echoServer.Install (csmaNodes.Get (nCsma));
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
   serverApps.Start (Seconds (1.0));
   serverApps.Stop (Seconds (10.0));
 
-  UdpEchoClientHelper echoClient (csmaInterfaces.GetAddress (nCsma), 9);
+  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
   echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-  ApplicationContainer clientApps = echoClient.Install (p2pNodes.Get (0));
+  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
   clientApps.Start (Seconds (2.0));
   clientApps.Stop (Seconds (10.0));
+  
+  // Enable PCAP tracing
+  pointToPoint.EnablePcapAll("firstScriptExample");
 
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-
-  pointToPoint.EnablePcapAll ("ptop");
-  csma.EnablePcap ("ptop", csmaDevices.Get (1), true);
-
-MobilityHelper mobility;
-mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-mobility.Install(p2pNodes);
-mobility.Install(csmaNodes);
-
-AnimationInterface anim("ptop.xml");
-AnimationInterface::SetConstantPosition (p2pNodes.Get(0), 10, 25);
-AnimationInterface ::SetConstantPosition(p2pNodes.Get(1), 20,25);
-AnimationInterface ::SetConstantPosition(csmaNodes.Get(1),40,25);
-AnimationInterface ::SetConstantPosition(csmaNodes.Get(2),50,25);
-AnimationInterface ::SetConstantPosition(csmaNodes.Get(3),60,25);
-anim.EnablePacketMetadata(true);
+  // Enable NetAnim tracing
+  AnimationInterface anim("firstScriptExample.xml");
 
   Simulator::Run ();
-  Simulator::Destroy ();
+  Simulator::Destroy (); 
+  
   return 0;
 }
